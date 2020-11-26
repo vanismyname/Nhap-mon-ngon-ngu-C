d71b5247a397b6b941b58bce4cf75af7d202085b
vanismyname <72543412+vanismyname@users.noreply.github.com>
2020-11-26 03:03:50 -0700

    Create chuvi.c

---
+++ chuvi.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+//khai bao bien R kieu double va gan gia tri R= 2.5
+double R = 2.5;
+//khai bao hang ten PI kieu double voi gia tri 3.14
+const double PI = 3.14;
+//khai bao hai bien CV va DT kieu duoble
+duoble CV = 2*R*PI;
+duoble D =R*R*P;
+printf("hien thi hai cot:cot thu nhat tieu de hang 1 la chu vi va hang 2la gia tri bien CV;cot thu hai tieu de hang 1 la DIEN TICH va hang 2 la gia tri bien DT")
+getchar();
+return 0;
+}
