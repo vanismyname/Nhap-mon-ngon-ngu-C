2af3bc1f47f5d038829142f59f3acb39da76236f
vanismyname <72543412+vanismyname@users.noreply.github.com>
2020-11-25 23:55:14 -0700

    Create pheptinh.c

---
+++ Bài tập 1/pheptinh.c
@@ -0,0 +1,18 @@
+#include<stdio>;
+
+int main(void) {
+
+int a,b;
+a = 3;
+b = 5;
+int tong, hieu, tich;
+thuong(double);
+tong = a + b;
+hieu = a - b;
+tich = a * b;
+thuong = (double)a\b;
+//hien thi ra man hinh tong = 8, hieu = -2, tich = 15, thuong = 0.6
+printf ("a + b =%d , a - b =%d , a * b =%d , a \ b =%0.1f"tong tong, thuong, hieu, tich );
+getchar();
+return 0;
+}
