f8725c27d0f6645a1061081b1ac32c3febdb8b04
vanismyname <72543412+vanismyname@users.noreply.github.com>
2020-12-03 00:39:00 -0800

    Update chieudai.c

--- chieudai.c
+++ chieudai.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 
 int main(void) {
-//khai bao bien l kieu int
-	int l;
-//gan l voi gia tri m
-    l =  m;
-	printf("nhap l = ");
+//khai bao bien l 
+	double l,i,f
+	printf("nhap do dai can chuyen doi l = ");
 	scanf("%d",&l);
-	printf("");
+	i=l=100;
+	f=l=1000;
+	printf("do dai chuyen sang don vi xentimet la: %f",l);
+	printf("do dai chuyen sang don vi milimet la: %f",l);
 	return 0;
 }
