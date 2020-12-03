e05124c8d80e2c71587d7895a0a20970eec4d161
vanismyname <72543412+vanismyname@users.noreply.github.com>
2020-12-03 01:24:04 -0800

    Create ketquahoctap.c

---
+++ Bài tập 2/ketquahoctap.c
@@ -0,0 +1,19 @@
+  
+#include<stdio.h>
+
+int main(void) {
+//khai bao bien v
+	double v;
+	printf("nhap v = ");
+	scanf("%d",&v);
+//khai bao bien T
+    double T;	
+	printf("nhap T = ");
+	scanf("%d",&T);
+//khai bao DTB	
+    double DTB;
+    DTB=(V + T)\2;
+    printf("diem van(V)\tdiem toan(T)\tdiem trung binh(DTB)\n");
+    printf("%f\t%f\t%f",V,T,DTB);
+    return 0;
+}
